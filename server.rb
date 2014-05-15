8ce03b39ab5c31203e8fa0b4eb83639752722c4c
Christopher Vollick <0@psycoti.ca>
2014-05-15 07:09:36 -0400

    Handle Login
    Now the user goes to a login page, and when they login it works.

--- server.rb
+++ server.rb
@@ -4,6 +4,12 @@ require 'openid/store/filesystem'
 
 include OpenID::Server
 
+enable :sessions
+
+def logged_in?
+	session[:user] && session[:user] == 'meow'
+end
+
 def server
 	if @server.nil?
 		dir = Pathname.new(File.dirname($0)).join('db').join('openid-store')
@@ -30,19 +36,16 @@ def respond(oidresp)
 	end
 end
 
-def handle(params)
-	begin
-		oidreq = server.decode_request(params)
-	rescue ProtocolError => e
-		return [500, e.to_s]
-	end
-
+def handle(oidreq)
 	oidresp = nil
 	if oidreq.kind_of?(CheckIDRequest)
 		if (oidreq.id_select)
 			oidresp = oidreq.answer(false)
-		else
+		elsif logged_in?
 			oidresp = oidreq.answer(true)
+		else
+			session[:last_req] = oidreq
+			return [302, {:Location => "/login"}, "Login"]
 		end
 	else
 		oidresp = server.handle_request(oidreq)
@@ -52,11 +55,49 @@ def handle(params)
 end
 
 get '/' do
-	handle(params)
+	begin
+		oidreq = server.decode_request(params)
+	rescue ProtocolError => e
+		return [500, e.to_s]
+	end
+	handle(oidreq)
 end
 
 post '/' do
-	handle(params)
+	begin
+		oidreq = server.decode_request(params)
+	rescue ProtocolError => e
+		return [500, e.to_s]
+	end
+	handle(oidreq)
+end
+
+def logged_in
+	'<html><head><title>Login</title></head><body>Neat.<form method="post" action="/logout"><input type="submit" value="Logout" /></form></body></html>'
+end
+
+get '/login' do
+	if logged_in?
+		logged_in
+	else
+		'<html><head><title>Login</title></head><body>Sup Bra?<form method="post"><input type="submit" value="Login" /></form></body></html>'
+	end
+end
+
+post '/logout' do
+	session[:user] = nil
+	[302, {:Location => '/login'}, "Logging Out"]
+end
+
+post '/login' do
+	session[:user] = 'meow'
+	if session[:last_req].nil?
+		logged_in
+	else
+		oidreq = session[:last_req]
+		session[:last_req] = nil
+		handle(oidreq)
+	end
 end
 
 get '/user/' do
